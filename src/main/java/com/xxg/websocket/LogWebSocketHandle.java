45bcf5711d9aabcc2853af7d7f58d025b269516c
yuzhencheng <yuzhencheng@focoon>
2016-01-26 17:52:12 +0800

    加入配置化

--- src/main/java/com/xxg/websocket/LogWebSocketHandle.java
+++ src/main/java/com/xxg/websocket/LogWebSocketHandle.java
@@ -1,7 +1,10 @@
 package com.xxg.websocket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ResourceBundle;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -14,20 +17,37 @@ public class LogWebSocketHandle {
 	
 	private Process process;
 	private InputStream inputStream;
-	
+	private static ResourceBundle rb = ResourceBundle.getBundle("config");
 	/**
 	 * 新的WebSocket请求开启
 	 */
 	@OnOpen
-	public void onOpen(Session session) {
+	public void onOpen(final Session session) {
 		try {
 			// 执行tail -f命令
-			process = Runtime.getRuntime().exec("tail -f /var/log/syslog");
+
+			process = Runtime.getRuntime().exec("tail -f  "+rb.getString("logfile"));
 			inputStream = process.getInputStream();
 			
 			// 一定要启动新的线程，防止InputStream阻塞处理WebSocket的线程
-			TailLogThread thread = new TailLogThread(inputStream, session);
-			thread.start();
+			new  Thread(){
+
+					BufferedReader reader= new BufferedReader(new InputStreamReader(inputStream));
+					@Override
+					public void run() {
+					String line;
+					try {
+						while((line = reader.readLine()) != null) {
+							// 将实时日志通过WebSocket发送给客户端，给每一行添加一个HTML换行
+							if(!line.contains(rb.getString("ignore")))
+							    session.getBasicRemote().sendText(line + "<br>");
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}.start();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
